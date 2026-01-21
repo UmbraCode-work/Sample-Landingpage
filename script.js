f638b4fa8f565ffc173434450b4fb7056285de9a
UmbraCode <211125340+UmbraCode-work@users.noreply.github.com>
2026-01-22 05:57:23 +0700

    fix color issues

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const navBar = document.getElementById('site-header');
-const openButton = document.querySelector('.open-sidebar-button');
-const navLinks = navBar.querySelectorAll('');
-const media = window.matchMedia("(width < 700px)");
+const navBar = document.getElementById("site-header");
+const openButton = document.querySelector(".open-sidebar-button");
+const navLinks = navBar.querySelectorAll("a");
+const media = window.matchMedia("max-width:768px)");
 
-media.addEventListener('change', (e) => updateNavBar(e))
+media.addEventListener("change", (e) => updateNavBar(e));
 
 function updateNavBar(e) {
-    const isMobile = e.matches;
-    if(isMobile){
-        navBar.setAttribute('inert', '');
-    }
-    else{
-        navBar.removeAttribute('inert');
-    }
-}   
-
-function openSidebar(){
-    navBar.classList.add('show');
-    openButton.setAttribute('aria-expanded', 'true');
-    navBar.removeAttribute('inert');
+  const isMobile = e.matches;
+  if (isMobile) {
+    navBar.setAttribute("inert", "");
+  } else {
+    navBar.removeAttribute("inert");
+  }
 }
 
-function closeSidebar(){
-    navBar.classList.remove('show');
-    openButton.setAttribute('aria-expanded', 'false');
-    navBar.setAttribute('inert', '');
+function openSidebar() {
+  navBar.classList.add("show");
+  openButton.setAttribute("aria-expanded", "true");
+  navBar.removeAttribute("inert");
 }
 
-navLinks.forEach(link => {
-    link.addEventListener('click', closeSidebar)})
+function closeSidebar() {
+  navBar.classList.remove("show");
+  openButton.setAttribute("aria-expanded", "false");
+  navBar.setAttribute("inert", "");
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeSidebar);
+});
 
 updateNavBar(media);
