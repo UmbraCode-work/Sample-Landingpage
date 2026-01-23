486018e9d797e8459518ad443ebc373578d0b904
UmbraCode <211125340+UmbraCode-work@users.noreply.github.com>
2026-01-23 16:38:30 +0700

    polishment before displaying on Fastwork

--- Ember/script.js
+++ Ember/script.js
@@ -1,8 +1,9 @@
 const navBar = document.getElementById("site-header");
-const openButton = document.querySelector(".open-sidebar-button");
-const navLinks = navBar.querySelectorAll("a");
-const media = window.matchMedia("max-width:768px)");
-
+const openSidebarButton = document.querySelector(".open-sidebar-button");
+const closeSidebarButton = document.querySelector(".close-sidebar-button");
+const navLinks = document.querySelectorAll("nav a");
+const overlay = document.querySelector("#overlay");
+const media = window.matchMedia("(max-width: 768px)");
 media.addEventListener("change", (e) => updateNavBar(e));
 
 function updateNavBar(e) {
@@ -11,23 +12,35 @@ function updateNavBar(e) {
     navBar.setAttribute("inert", "");
   } else {
     navBar.removeAttribute("inert");
+    navBar.classList.remove("show");
+    openSidebarButton.setAttribute("aria-expanded", "false");
   }
 }
 
 function openSidebar() {
+  if (!media.matches) return;
   navBar.classList.add("show");
-  openButton.setAttribute("aria-expanded", "true");
+  openSidebarButton.setAttribute("aria-expanded", "true");
   navBar.removeAttribute("inert");
 }
 
 function closeSidebar() {
+  if (!media.matches) return;
   navBar.classList.remove("show");
-  openButton.setAttribute("aria-expanded", "false");
+  openSidebarButton.setAttribute("aria-expanded", "false");
   navBar.setAttribute("inert", "");
 }
 
 navLinks.forEach((link) => {
   link.addEventListener("click", closeSidebar);
 });
+
+document.addEventListener("keydown", (e) => {
+  if (e === "Escape" && navBar.classList.contains("show")) closeSidebar();
+});
+
+openSidebarButton.addEventListener("click", openSidebar);
+closeSidebarButton.addEventListener("click", closeSidebar);
+overlay.addEventListener("click", closeSidebar);
 
 updateNavBar(media);
